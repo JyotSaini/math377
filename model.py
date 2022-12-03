f8bd70c45e1f4f4daf029107259802082263807e
Jay Saini <jyotsaini@icloud.com>
2022-12-02 19:26:41 -0800

    Delete comments

--- model.py
+++ model.py
@@ -13,10 +13,6 @@ G = nx.Graph()
 
 for donor in donors.keys():             # First iteration to declare nodes in graph
     alive = None
-    # donor_mat = []
-
-    # for n in range(len(donors)):
-    #     donor_mat.append(100)
 
     for recipient in donors[donor].keys():
         if recipient == 'alive':
@@ -27,17 +23,10 @@ for donor in donors.keys():             # First iteration to declare nodes in gr
     else:
         G.add_node(donor, label='Deceased')
 
-    # donors_mat.append(donor_mat)
-
 for donor in donors.keys():             # Second iteration to declare edges in graph
     for recipient in donors[donor].keys():
         if recipient != 'alive':
-            G.add_edge(donor, recipient, weight=(donors[donor][recipient]/10))      # We're scaling down the weight to make the graph look nicer
-            # donors_mat[int(donor[1:])-1][int(recipient[1:])-1] = donors[donor][recipient]
-
-# Deceased donor columns
-# for donor in range(len(donors) - (len(donors['d1']) - 1)):
-#     donors_mat[0][len(donors) - 1 - donor] = 0
+            G.add_edge(donor, recipient, weight=(donors[donor][recipient]))      # We're scaling down the weight to make the graph look nicer
 
 g = Network(height=500, width="100%", bgcolor="#222222", font_color="white", notebook=True)
 # g = Network()
@@ -69,7 +58,7 @@ const options = {
 """)
 g.show("before.html")
 
-intermediate = nx.min_weight_matching(G)
+intermediate = nx.max_weight_matching(G)
 newset = set()
 toremove = set()
 for edge in intermediate:
